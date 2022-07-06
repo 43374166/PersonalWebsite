65d04f0e4dc84d62146ba11e8a06dc4fb2c8f96b
凌磊 <320043337@qq.com>
2022-07-06 17:32:45 +0800

    auto deploy script

---
+++ script/autoDeploy.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+const path = require('path');
+const { NodeSSH } = require('node-ssh');
+const localPath = path.join(__dirname, '../dist');
+const config = require('./serverConfig.js');
+const remotePath = config.remotePath; // 部署路径
+
+const ssh = new NodeSSH();
+
+// 获取当前git分支
+const exec = require('child_process').execSync;
+const name = exec('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim();
+if (name !== 'master') return console.log('⚠️⚠️⚠️请确保在 "master" 分支发布');
+
+ssh.connect({
+    host: config.host,
+    port: config.port,
+    username: config.username,
+    password: config.password,
+  })
+  .then(() => {
+    console.log('😜SSH connect success!');
+    const failed = [];
+    const successful = [];
+    ssh.putDirectory(localPath, remotePath, {
+        recursive: true,
+        concurrency: 10,
+        validate(itemPath) {
+          const baseName = path.basename(itemPath);
+          return (
+            baseName.substr(0, 1) !== '.' && baseName !== 'node_modules' // do not allow dot files
+          ); // do not allow node_modules
+        },
+        tick(localPath, remotePath, error) {
+          if (error) {
+            failed.push(localPath);
+          } else {
+            successful.push(localPath);
+          }
+        },
+      })
+      .then(res => {
+        console.log('😜部署开始');
+        for (let i = 0; i < successful.length; i++) {
+          console.log(successful[i]);
+        }
+        console.log('👀部署结束');
+        console.log('💩部署失败文件数：' + failed.length, failed.join(', '));
+        console.log('😜部署成功文件数：' + successful.length);
+        process.exit();
+      });
+  });
---
+++ script/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: '159.138.57.207',
+  port: 22,
+  username: 'root',
+  password: '@LicoLing123',
+  remotePath: '/var/www/html', // 部署路径
+};
